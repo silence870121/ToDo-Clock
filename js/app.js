e642b0574ee9eaf2a70c1764999a9812598289a9
Herbert (WEI-CHE, Chung) <silence870121@gmail.com>
2021-10-06 17:37:53 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -35,25 +35,24 @@ Vue.createApp({
                 date_create: "yyyy/mm/dd", //建立日期
                 date_limit: { //項目期限
                     year: 2021,
-                    month: 09,
-                    day: 08,
+                    month: 9,
+                    day: 8,
                 },
                 date_complete: "yyyy/mm/dd", //完成日期
             },
-
             //? Working Task
             working: {
-                id: "", //項目建立時之時間戳
+                id: "000", //項目建立時之時間戳
                 title: "項目名稱",
                 clock_expect: 5, //預期時間
                 clock_spend: 3, //花費時間
                 clock_over: 0, //超出時間
-                completed: true, //項目完成狀態 完成: true | 未完成: false
+                completed: false, //項目完成狀態 完成: true | 未完成: false
                 date_create: "yyyy/mm/dd", //建立日期
                 date_limit: { //項目期限
                     year: 2021,
-                    month: 09,
-                    day: 08,
+                    month: 9,
+                    day: 8,
                 },
                 date_complete: "yyyy/mm/dd", //完成日期
             },
@@ -168,6 +167,23 @@ Vue.createApp({
                     break;
             }
         },
+        resetClockData() {
+            this.working = {
+                id: "000", //項目建立時之時間戳
+                title: "無指定項目",
+                clock_expect: 8, //預期時間
+                clock_spend: 0, //花費時間
+                clock_over: 0, //超出時間
+                completed: false, //項目完成狀態 完成: true | 未完成: false
+                date_create: null, //建立日期
+                date_limit: { //項目期限
+                    year: 2021,
+                    month: 9,
+                    day: 8,
+                },
+                date_complete: "yyyy/mm/dd", //完成日期
+            }
+        },
         renderClock() {
             if (this.clock.time % 60 < 10) {
                 this.clock.timerText = `${parseInt(this.clock.time / 60)}:0${this.clock.time % 60}`
@@ -188,7 +204,11 @@ Vue.createApp({
         },
         playingClock() {
             if (this.clock.playing) {
-                this.clock.time--
+                if (this.working == null) {
+                    this.resetClockData()
+                }
+                this.clock.time--;
+                localStorage.setItem('clock', JSON.stringify(this.working))
             }
             if (this.clock.time <= 0) {
                 this.switchClock()
@@ -242,5 +262,6 @@ Vue.createApp({
         this.timer = setInterval(this.playingClock, 1000);
         this.setClockTime()
         this.renderClock()
+        this.working = JSON.parse(localStorage.getItem('clock'));
     }
 }).mount('#app');
